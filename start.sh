7bc34184453b0041b83c198633379c693c622fd3
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-11-03 11:19:28 +0300

    updated requirements

---
+++ start.sh
@@ -0,0 +1,6 @@
+export DATABASE_URI ='postgresql+psycopg2://moringa:lekam2020@localhost/blogtechnology'
+export SECRET_KEY='LEKAM'
+export MAIL_USERNAME='charitylekam@gmail.com'
+export MAIL_PASSWORD='kulangombe'
+
+python3.8 manage.py server
