5f961285d219bf9aeff0ac591bd9c246c7ade67a
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-11-04 22:42:11 +0300

    deployment to heroku

--- app/main/forms.py
+++ app/main/forms.py
@@ -13,12 +13,10 @@ class BlogForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class CommentForm(FlaskForm):
-
     comment = TextAreaField('Comment')
     submit = SubmitField('Post Comments')
 
 class SubscriberForm(FlaskForm):
-
     email = StringField('Your Email Address')
     name = StringField('Enter your name',validators = [Required()])
     submit = SubmitField('Subscribe')
