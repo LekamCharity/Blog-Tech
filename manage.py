74b68400fb75097fb74be502d8f9667a40d593d3
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-11-05 12:42:14 +0300

    new changes

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 from app.models import *
 
-
 app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server(use_debugger=True))
